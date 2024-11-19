f6a04451bc2ba0654278485eda498827ae66e547
chrisdograu <128731557+chrisdograu@users.noreply.github.com>
2024-11-19 01:23:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function altername(){
+    var l = document.getElementById("links")
+    if(l.style.display=="flex"){
+      l.style.display="none";
+    }
+    else{
+        l.style.display="flex";
+    }
+    }
+    
